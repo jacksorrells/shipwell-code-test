81afebd9a03115cd0e51c9934b15e7218549aa67
Jack Sorrells <jack@jacksorrells.com>
2019-11-02 17:47:36 -0500

    adding some redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,8 @@ export const ADD_STOP = "ADD_STOP";
 export const EDIT_STOP = "EDIT_STOP";
 export const DELETE_STOP = "DELETE_STOP";
 export const COMPLETE_STOP = "COMPLETE_STOP";
-export const VALIDATE_ADDRESS = "VALIDATE_ADDRESS";
-export const VALIDATE_ADDRESS_SUCCESS = "VALIDATE_ADDRESS_SUCCESS";
-export const VALIDATE_ADDERSS_FAILURE = "VALIDATE_ADDRESS_SUCCESS";
-export const RECEIVE_ADDRESS_VALIDATION = "RECEIVE_ADDRESS_VALIDATION";
+export const REQUEST_ADDRESS_VALIDATION = "REQUEST_ADDRESS_VALIDATION";
+export const RECEIVE_ADDRESS_VALIDATION = "RECEIVE_ADDRESS_VALIDATION"
 
 export const addStop = stop => ({
   type: ADD_STOP,
@@ -27,15 +25,36 @@ export const completeStop = stop => ({
   stop
 });
 
+export const requestAddressValidation = stop => ({
+  type: REQUEST_ADDRESS_VALIDATION,
+  stop
+});
+
+export const receiveAddressValidation = (stop, json) => ({
+  type: RECEIVE_ADDRESS_VALIDATION,
+  stop,
+  stopValidationInfo: json.data,
+});
+
 const fetchStopValidation = stop => dispatch => {
-  return fetch(`${stop.address}`)
+  const url = "https://dev-api.shipwell.com/v2/locations/addresses/validate/";
+  const data = { formatted_address: stop.address };
+
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache"
+    },
+    body: JSON.stringify(data)
+  })
     .then(response => response.json())
-    .then(json => dispatch(receivePosts(stop, json)));
+    .then(json => dispatch(receiveAddressValidation(stop, json)));
 }
 
 const shouldFetchStopValidation = (state, stop) => {
   const stops = state.stops;
-  if(!stops) {
+  if (!stops) {
     return true
   }
   if (stops.isFetching) {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,58 @@
 import { combineReducers } from 'redux';
+import {
+  ADD_STOP,
+  EDIT_STOP,
+  DELETE_STOP,
+  COMPLETE_STOP,
+  REQUEST_ADDRESS_VALIDATION,
+  RECIEVE_ADDRESS_VALIDATION,
+} from '../actions';
 
-
-
-const itinerary = (state = [], action) => {
+const stop = (
+  state = {
+    isFetching: false,
+    stopName: '',
+    formattedAddress: '',
+    isCompleted: false
+  }, action
+) => {
   switch(action.type) {
+    case EDIT_STOP:
+      return;
+    case COMPLETE_STOP:
+      return;
+    case DELETE_STOP:
+      return;
+    default:
+      return state;
+  }
+}
 
+const itinerary = (
+  state = {
+    stops: [],
+  },
+  action
+) => {
+  switch (action.type) {
+    case ADD_STOP:
+      return;
+    case EDIT_STOP:
+      return;
+    case DELETE_STOP:
+      return;
+    case COMPLETE_STOP:
+      return;
+    case REQUEST_ADDRESS_VALIDATION:
+      return {
+        ...state,
+        isFetching: true
+      };
+    case RECIEVE_ADDRESS_VALIDATION:
+      return {
+        ...state,
+        isFetching: false,
+      };
     default:
       return state;
   }
